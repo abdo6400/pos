dc5e7a608b9d70301a24ae8ec0eed725919e721b
abdo6400 <Abdulrahmanamr348@gmail.com>
2024-12-24 18:06:50 +0200

    fix scroll in login form

--- lib/features/auth/presentation/screens/login_screen.dart
+++ lib/features/auth/presentation/screens/login_screen.dart
@@ -19,55 +19,61 @@ class LoginScreen extends StatelessWidget {
     return SafeArea(
       child: BlocProvider(
         create: (context) => locator<LoginBloc>(),
-        child: Scaffold(
-          body: Stack(
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage(Assets.loginBackground),
-                    fit: BoxFit.fill,
-                  ),
+        child: Stack(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(Assets.loginBackground),
+                  fit: BoxFit.fill,
                 ),
               ),
-              Center(
-                child: SizedBox(
-                  width: context.ResponsiveValu(300,
-                      mobile: 200, tablet: 500, desktop: 600),
-                  child: BlocConsumer<LoginBloc, LoginState>(
-                    listener: (context, state) async {
-                      if (state is LoginSuccess) {
-                        await storage.saveAuthTokenState(
-                            state.tokens.token, state.tokens.toJson());
-                        _formKey.currentState?.reset();
-                        context.hideOverlayLoader();
-                        context.pushReplacement(AppRoutes.main);
-                      } else if (state is LoginError) {
-                        context.hideOverlayLoader();
-                        context.showMessageToast(msg: state.message);
-                      } else if (state is LoginLoading) {
-                        context.showLottieOverlayLoader(Assets.loader);
-                      }
-                    },
-                    builder: (context, state) => LoginFormBuilder(
-                      formKey: _formKey,
-                      onSubmit: () async => context.read<LoginBloc>().add(
-                            LoginEvent(
-                              email: _formKey.currentState
-                                  ?.fields[StringEnums.email.name]?.value,
-                              password: _formKey.currentState
-                                  ?.fields[StringEnums.password.name]?.value,
+            ),
+            Scaffold(
+              backgroundColor: Colors.transparent,
+              body: Container(
+                alignment: Alignment.center,
+                width: double.infinity,
+                height: double.infinity,
+                child: SingleChildScrollView(
+                  child: SizedBox(
+                    width: context.ResponsiveValu(300,
+                        mobile: 200, tablet: 500, desktop: 600),
+                    child: BlocConsumer<LoginBloc, LoginState>(
+                      listener: (context, state) async {
+                        if (state is LoginSuccess) {
+                          await storage.saveAuthTokenState(
+                              state.tokens.token, state.tokens.toJson());
+                          _formKey.currentState?.reset();
+                          context.hideOverlayLoader();
+                          context.pushReplacement(AppRoutes.main);
+                        } else if (state is LoginError) {
+                          context.hideOverlayLoader();
+                          context.showMessageToast(msg: state.message);
+                        } else if (state is LoginLoading) {
+                          context.showLottieOverlayLoader(Assets.loader);
+                        }
+                      },
+                      builder: (context, state) => LoginFormBuilder(
+                        formKey: _formKey,
+                        onSubmit: () async => context.read<LoginBloc>().add(
+                              LoginEvent(
+                                email: _formKey.currentState
+                                    ?.fields[StringEnums.email.name]?.value,
+                                password: _formKey.currentState
+                                    ?.fields[StringEnums.password.name]?.value,
+                              ),
                             ),
-                          ),
-                      onReset: () {},
-                      onLoginWithGoogle: () {},
+                        onReset: () {},
+                        onLoginWithGoogle: () {},
+                      ),
                     ),
                   ),
                 ),
               ),
-              CustomAppBar(),
-            ],
-          ),
+            ),
+            CustomAppBar(),
+          ],
         ),
       ),
     );
